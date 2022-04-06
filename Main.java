a9cd9b7dd52e30838c1ecaf15ba727b57ede0a5a
Kenn1690 <Kenneth@Strok>
2022-04-06 11:17:54 -0400

    first upload of data reversal

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int[] data1= {1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0,
+                1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0};
+        DataReverse(data1);
+    }
+    public static int[] DataReverse(int[] data) {
+        int totalLength = data.length;
+        int answer[] = new int[totalLength];
+        int counter = 0;
+        System.out.println(totalLength);
+        while(totalLength > 0){
+            for(int i = totalLength - 8; i < totalLength; i ++){
+                answer[counter] = data[i];
+                System.out.print(data[i]);
+                counter ++;
+            }
+            System.out.println();
+            totalLength -= 8;
+        }
+        return answer;
+    }
+}
